e14f9fa2e1ca22a0f95a208af1f8aed4ca07d69a
Qi Qi <qiqi789@gmail.com>
2014-10-16 22:13:20 +0800

    added lecture06 source.

---
+++ src/myScalaCourse/lecture06.scala
@@ -0,0 +1,23 @@
+package myScalaCourse
+
+object lecture06 {
+
+//  object A 
+//  object B extends A ; not legal
+  
+//  object A
+//  class B extends A ; not legal
+  
+//  class A {
+//    private def aaa = {}
+//    
+//  }
+//  val a = new A
+//  a.aaa  // private memebers cannot be accessed by its objects.
+  
+  
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+}
