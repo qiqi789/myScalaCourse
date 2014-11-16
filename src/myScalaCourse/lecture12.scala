f5bfbd306b39eac587d9b98b1abf2b4384a78c75
Qi Qi <qiqi789@gmail.com>
2014-11-16 21:15:28 +0800

    added lecture12's codes.

---
+++ src/myScalaCourse/lecture12.scala
@@ -0,0 +1,31 @@
+package myScalaCourse
+
+object lecture12 {
+
+  object ESieve {
+    def from(n : Int): Stream[Int] = n #:: from(n+1)
+    def sieve(s : Stream[Int]): Stream[Int] = 
+      s.head #:: sieve(s.tail filter (_ % s.head != 0))
+      
+  }
+  
+  def sqrtStream(x: Double): Stream[Double] = {
+    def improve(guess: Double) = (guess + x / guess) / 2
+    lazy val guesses: Stream[Double] = 1 #:: (guesses map improve)
+    guesses
+  }
+  
+  def isGoodEnough(guess: Double, x: Double) = 
+    math.abs((guess * guess - x)/x) < 0.0001
+  
+  def main(args: Array[String]): Unit = {
+    import ESieve._
+    
+    val primes = sieve(from(2))
+    println((primes take 6).toList)
+    
+    val s = sqrtStream(4) filter (isGoodEnough(_, 4))
+    println(s.head)
+  }
+
+}
